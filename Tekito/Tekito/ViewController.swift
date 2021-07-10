835430ec5e861786b76ad23461f9de2cf76f3107
Shinsuke0903 <83898517+Shinsuke0903@users.noreply.github.com>
2021-07-10 09:42:50 +0900

    課題8

---
+++ Tekito/Tekito/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tekito
+//
+//  Created by Shinsuke Goto on 2021/07/08.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+//    アプリ起動時に一度動くもの
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        テキストの変更
+       
+  
+
+       
+        
+    }
+
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBAction func yoyaku(_ sender: UIButton) {
+    }
+    
+}
+
